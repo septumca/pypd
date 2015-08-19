4c3d583085353bc6abc16d17b95f9e4522d1c9cc
Juraj Sebin <sebin.juraj@gmail.com>
2015-08-19 22:52:43 +0200

    Changed formating to confirm to pep8
    Removed unnecessary underscores

--- predicate_dispatch.py
+++ predicate_dispatch.py
@@ -1,48 +1,57 @@
 _conditional_callables = {}
 
+
 def _get_qualname(o):
     try:
         return o.__qualname__
     except AttributeError as _:
         return o.__module__+'.'+o.__class__.__name__+'.'+o.__name__
 
+
 def _default_condition(*args, **kwargs):
     return True
+
 
 def _get_callable(qual_name):
     return _conditional_callables.get(qual_name, ([], None))
 
+
 def _add_callable(condition, f):
-    _qual_name = _get_qualname(f)
-    _callable_tuple = _get_callable(_qual_name)
+    qual_name = _get_qualname(f)
+    callable_tuple = _get_callable(qual_name)
 
     if condition == _default_condition:
-        _callable_tuple = _callable_tuple[0], f
+        callable_tuple = callable_tuple[0], f
     else:
-        _callable_list = _callable_tuple[0]
-        _callable_list.append((condition, f))
-        _callable_tuple = _callable_list, _callable_tuple[1]
+        callable_list = callable_tuple[0]
+        callable_list.append((condition, f))
+        callable_tuple = callable_list, callable_tuple[1]
 
-    _conditional_callables[_qual_name] = _callable_tuple
+    _conditional_callables[qual_name] = callable_tuple
+
 
 def _resolve_callable(f, *args, **kwargs):
-    _qual_name = _get_qualname(f)
-    _callable_tuple = _get_callable(_qual_name)
-    _callable_iterator = (cc[1] for cc in _callable_tuple[0] if cc[0](*args, **kwargs))
-    _callable = next(_callable_iterator, _callable_tuple[1])
+    qual_name = _get_qualname(f)
+    callable_tuple = _get_callable(qual_name)
+    callable_iterator = (cc[1] for cc in callable_tuple[0]
+                         if cc[0](*args, **kwargs))
 
-    return _callable
+    return next(callable_iterator, callable_tuple[1])
+
 
 def predicate(condition=_default_condition):
     def wrapper(f):
         _add_callable(condition, f)
+
         def wrapped(*args, **kwargs):
-            _callable = _resolve_callable(f, *args, **kwargs)
-            if _callable is not None:
-                return _callable(*args, **kwargs)
+            resolved_callable = _resolve_callable(f, *args, **kwargs)
+            if resolved_callable is not None:
+                return resolved_callable(*args, **kwargs)
             else:
-                raise TypeError('Predicate for \'{fname}\' is not found'.format(fname=_get_qualname(f)))
+                raise TypeError('Predicate for \'{fname}\' is not found'
+                                .format(fname=_get_qualname(f)))
         return wrapped
+
     return wrapper
 
 
@@ -51,19 +60,19 @@ if __name__ == "__main__":
     def foo(v):
         return 'default'
 
-    @predicate(lambda v: v==1)
+    @predicate(lambda v: v == 1)
     def foo(v):
         return 'f1'
 
-    @predicate(lambda v: v==2)
+    @predicate(lambda v: v == 2)
     def foo(v):
         return 'f2'
 
-    @predicate(lambda v: v==3)
+    @predicate(lambda v: v == 3)
     def foo(v):
         return 'f3'
 
-    @predicate(lambda x, y: x==3 and y==2)
+    @predicate(lambda x, y: x == 3 and y == 2)
     def foo_multiple(x, y):
         return True
 
@@ -71,7 +80,7 @@ if __name__ == "__main__":
     def foo_multiple(x, y):
         return False
 
-    @predicate(lambda x: x>1)
+    @predicate(lambda x: x > 1)
     def factorial(x):
         return x*factorial(x-1)
 
@@ -79,7 +88,7 @@ if __name__ == "__main__":
     def factorial(x):
         return x
 
-    @predicate(lambda x: x==1)
+    @predicate(lambda x: x == 1)
     def bar(x):
         return True
 
